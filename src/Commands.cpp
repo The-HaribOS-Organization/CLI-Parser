c2b12c7a732185824fbc99b403abee93ad2f0d34
tarturr <tartur.dev@gmail.com>
2024-02-11 02:09:13 +0100

    Changed test command message

--- src/Commands.cpp
+++ src/Commands.cpp
@@ -8,6 +8,6 @@ TestCommand::TestCommand() noexcept : Command(Command::FixedCommandPattern("test
 
 Command<>::Output TestCommand::Run(const CommandContext &context) const noexcept
 {
-    std::cout << "Nice one." << std::endl;
+    std::cout << "Changed this message so that I can create a new pull request." << std::endl;
     return SUCCESS;
 }
